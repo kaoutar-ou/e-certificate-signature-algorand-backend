3dd2a7df4b2a937fe2517d76f3b2e18bc382c446
abderox <abdelhadi12mouzafir@gmail.com>
2022-10-11 03:27:21 +0100

    E-CERTIFICATE-SIGNATURE :  generate certifs for all students

--- api/routes/process.js
+++ api/routes/process.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {verifyToken,isSuperAdmin,isAdmin} = require("../middleware/auth");
 
-const { generateCertificate ,generateCertificateTest} = require("../controllers/process.controller")
+const { generateCertificate ,generateCertificateTest,generateForAllStudents} = require("../controllers/process.controller")
 
 router.post('/generate-certificate',[verifyToken,isAdmin], generateCertificate);
 router.post('/generate-certificate-test', generateCertificateTest);
+router.post('/generate-certificates', generateForAllStudents);
+
 
 module.exports = router;
